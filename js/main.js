f6e7ab685af3bfca5ecf0a1519921acce9a0dd62
hiro-49 <kazuki97101@gmail.com>
2019-10-19 15:58:44 +0900

    ファイルとか持ってくる

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+import anime from "animejs";
+
+{
+  var elem = document.getElementById('elem');
+  elem.addEventListener('click', function(){
+    anime({
+      targets: elem,
+      translateX: 250
+    })
+  })
+}
